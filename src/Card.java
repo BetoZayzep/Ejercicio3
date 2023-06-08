11c10acaad9056d950125e818d2d56aaf6b99496
BetoZayzep <134879851+BetoZayzep@users.noreply.github.com>
2023-06-07 20:23:14 -0600

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Card {
+public class Card {
+    private final String palo;   // El palo de la carta (tréboles, corazones, picas, diamantes)
+    private final String color;  // El color de la carta (rojo, negro)
+    private final String valor;  // El valor de la carta (2, 3, 4, ..., 10, A, J, Q, K)
+
+    public Card(String palo, String color, String valor) {
+        this.palo = palo;
+        this.color = color;
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return palo + "," + color + "," + valor;  // Representación de la carta en formato "palo,color,valor"
+    }
 }
